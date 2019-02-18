24e7459900010104b5b51bf9c6d4c8a50e52af75
neocode24 <neocode24@gmail.com>
2019-02-18 15:48:13 +0900

    updated file

--- retrieve-usagedata-svc/src/main/java/com/garage/usagedata/RetrieveUsagedataSvcApplication.java
+++ retrieve-usagedata-svc/src/main/java/com/garage/usagedata/RetrieveUsagedataSvcApplication.java
@@ -1,20 +1,15 @@
 package com.garage.usagedata;
 
-import java.time.Duration;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import java.time.Duration;
 
-@EnableSwagger2
+@Slf4j
 @SpringBootApplication
 public class RetrieveUsagedataSvcApplication {
 
@@ -30,15 +25,5 @@ public class RetrieveUsagedataSvcApplication {
 				.setReadTimeout(Duration.ofSeconds(30 * 1000))
 				.build();
 	}
-	
-	@Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-
-    }
 }
 
---
+++ retrieve-usagedata-svc/src/main/java/com/garage/usagedata/config/SwaggerConfig.java
@@ -0,0 +1,48 @@
+package com.garage.usagedata.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+
+@Slf4j
+@Configuration
+@Profile({"swagger"})
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.garage"))
+                .paths(PathSelectors.ant("/**"))
+                .build()
+                .apiInfo(getApiInfo());
+
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfo(
+                "KT MSA 플랫폼 구축 프로젝트",
+                "API Documents",
+                "1.0",
+                null,
+                null,
+                null,
+                null,
+                Collections.emptyList()
+        );
+    }
+
+}
